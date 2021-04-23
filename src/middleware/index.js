9974a3d7e8bbebb8082925a061c276bb0970caff
Luiz Gustavo <luiz.matias@upe.br>
2021-04-23 11:08:26 -0300

    feat(middleware): adding jsdocs in the middleware method

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,18 @@
-export function checkSession(req, res, next) {
+import { Request, Response, NextFunction } from "express";
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+export const checkSession = (req, res, next) => {
   const existSession = req.get("Authorization");
+  const endpoint = req.originalUrl;
 
-  if (existSession) {
-    return next();
-  }
+  if (existSession) return next();
 
   return res.status(400).json({
-    endpoint: req.originalUrl,
+    endpoint,
     msg: "login necessary to see this page",
   });
-}
+};
