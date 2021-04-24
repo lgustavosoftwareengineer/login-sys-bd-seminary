eb7cc84f7ffb39f064aa14489d7afe1ce466e3c1
Luiz Gustavo <luiz.matias@upe.br>
2021-04-24 15:16:03 -0300

    refactor(server): changing the name of the const app to server

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 import { authController, homeController } from "./controllers";
 import { checkSession } from "./middleware";
 
-const app = express();
-app.use(express.json());
+const server = express();
+server.use(express.json());
 
 try {
   mongoose.connect("mongodb://localhost/login-sys-test", {
@@ -16,9 +16,9 @@ try {
   console.log(error);
 }
 
-app.post("/v1/auth/register", authController.register);
-app.post("/v1/auth/login", authController.login);
-app.delete("/v1/auth/logout", authController.logout);
-app.get("/v1/home/user-details", checkSession, homeController.userDetails);
+server.post("/v1/auth/register", authController.register);
+server.post("/v1/auth/login", authController.login);
+server.delete("/v1/auth/logout", authController.logout);
+server.get("/v1/home/user-details", checkSession, homeController.userDetails);
 
-app.listen(3000);
+server.listen(3000);
